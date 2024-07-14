dc9ea48ddcee562d42a9e5ae79cb6ee10bf2c7a7
BrunoCardoso02 <bcardoso@euimob.com.br>
2024-07-14 13:47:21 -0300

    Função de criar novo usuário criada

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response} from 'express';
+import UserController from './controllers/UserController';
 
 const app = express();
 const port = 8000;
@@ -10,6 +11,8 @@ app.get('/', (req: Request, res: Response) => {
     message: 'Hello world',
   });
 });
+
+app.post('/createUser', UserController.createUser);
 
 app.listen(port, () => {
   console.log(`App running on localhost: ${port}`)
